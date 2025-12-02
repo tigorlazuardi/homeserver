371250d665daca911c65a6f618b6d359ef98e92c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-02 21:42:31 +0700

    feat(nixos): add shared home-manager modules to mkNixosConfiguration
    Add home-manager, nix-index-database, and sops-nix to common modules.

--- nixos/default.nix
+++ nixos/default.nix
@@ -6,6 +6,17 @@ let
       modules = [
         module
         ./common/system
+        inputs.home-manager.nixosModules.home-manager
+        inputs.nix-index-database.nixosModules.nix-index
+        {
+          programs.nix-index-database.comma.enable = true;
+          home-manager.sharedModules = [
+            inputs.sops-nix.homeManagerModules.sops
+            {
+              sops.age.keyFile = "/opt/age-key.txt";
+            }
+          ];
+        }
       ];
       specialArgs = { inherit inputs; };
     };
