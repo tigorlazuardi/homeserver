a765cd359d3f92bb1ffcdc54da89e8d37e592cc4
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-03 00:00:35 +0700

    fix(homeserver): enable systemd-networkd properly
    Add networking.useNetworkd = true to fix evaluation warning about
    conflicting network managers. Remove unused networkmanager extraGroups.

--- nixos/homeserver/networking.nix
+++ nixos/homeserver/networking.nix
@@ -1,13 +1,24 @@
 {
   # Unless we have multiple network cards, we don't need this to be true.
   networking.usePredictableInterfaceNames = false;
-  networking.networkmanager.enable = true;
+  networking.useNetworkd = true;
+
   users.users.homeserver = {
-    extraGroups = [ "networkmanager" ];
-    authorizedKeys.keys = [
+    openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPO1aSG3/1vrgEPgK038tZ8+ipz3gZqr9hRT0JUteJXY tigor@fort"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/dGHD56+3qsLhUvmG4GeN8JrpYw7oGt0iQT+WkZzFu tigor@nexus"
     ];
   };
+
+  systemd.network = {
+    enable = true;
+    networks."25-wired" = {
+      matchConfig.Name = "eth0";
+      networkConfig.DHCP = "ipv4";
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
+
   networking.hostName = "homeserver";
   networking.enableIPv6 = false;
 
