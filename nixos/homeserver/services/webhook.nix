3a14f8dfa8414d99cc41f54da9859331a5d76afe
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-03 06:09:25 +0000

    feat(webhook): disabled if there's no settings added

--- nixos/homeserver/services/webhook.nix
+++ nixos/homeserver/services/webhook.nix
@@ -1,10 +1,11 @@
+{ config, ... }:
 {
   # Webhook will be executed as root to allow it to restart services.
   #
   # Do be careful when creating scripts that are triggered by webhooks as they
   # will have full root access to the system.
   services.webhook = {
-    enable = true;
+    enable = (config.services.webhook.hooks != {});
     user = "root";
     group = "root";
   };
