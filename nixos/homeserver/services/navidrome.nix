0170aea850746fe6ea551b6a9a2dee0f5c7e9ca3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-04 16:02:30 +0000

    feat(homeserver): add navidrome music server service

---
+++ nixos/homeserver/services/navidrome.nix
@@ -0,0 +1,48 @@
+# Navidrome - Modern Music Server and Streamer
+# https://www.navidrome.org/docs/installation/docker/
+{ config, ... }:
+let
+  inherit (config.virtualisation.oci-containers.containers.navidrome) ip httpPort;
+  domain = "navidrome.tigor.web.id";
+  dataDir = "/var/lib/navidrome";
+  musicDir = "/var/mnt/nas/Syncthing/Sync/Music";
+in
+{
+  virtualisation.oci-containers.containers.navidrome = {
+    image = "deluan/navidrome:latest";
+    ip = "10.88.1.11";
+    httpPort = 4533;
+    autoStart = true;
+    user = "1000:1000";
+    volumes = [
+      "${dataDir}:/data"
+      "${musicDir}:/music:ro"
+    ];
+    environment = {
+      TZ = "Asia/Jakarta";
+      ND_SCANSCHEDULE = "1h";
+      ND_LOGLEVEL = "info";
+      ND_SESSIONTIMEOUT = "24h";
+      ND_BASEURL = "";
+    };
+  };
+
+  systemd.services.podman-navidrome = {
+    preStart = ''
+      mkdir -p ${dataDir}
+      chown 1000:1000 ${dataDir}
+    '';
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "5s";
+    };
+  };
+
+  services.nginx.virtualHosts.${domain} = {
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://${ip}:${toString httpPort}";
+      proxyWebsockets = true;
+    };
+  };
+}
