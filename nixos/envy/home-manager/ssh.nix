d8b7ae1f5d4e84bebbd449916033d2d82919745f
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-05 10:00:08 +0700

    feat(envy): applied permanent ssh keys

--- nixos/envy/home-manager/ssh.nix
+++ nixos/envy/home-manager/ssh.nix
@@ -1,3 +1,13 @@
+{ config, ... }:
 {
-  # TODO Fill this
+  sops.secrets = {
+    "ssh/private_key" = {
+      sopsFile = ./ssh.yaml;
+      path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+    };
+    "ssh/public_key" = {
+      sopsFile = ./ssh.yaml;
+      path = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+    };
+  };
 }
