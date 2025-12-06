702c936aea2ec69b5f6259688afb7472963e23b8
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-06 21:05:10 +0700

    feat(ytptube): add title filter for ASMR content and kimitsutenka channel

--- nixos/homeserver/services/ytptube/subscribe.nix
+++ nixos/homeserver/services/ytptube/subscribe.nix
@@ -3,7 +3,21 @@
   services.flexget.settings =
     let
       script = pkgs.writers.writeJS "rss-feeder" { } ''
-        const url, folder = process.argv[2], process.argv[3]
+        const filters = ["ASMR", "KU-100", "KU100"]
+        const title = process.argv[2]
+        const url = process.argv[3]
+        const folder = process.argv[4]
+        let hasMatch = false
+        for (const filter of filters) {
+          if (title.includes(filter)) {
+            hasMatch = true
+            break
+          }
+        }
+        if (!hasMatch) {
+          console.error("Skipping non-matching title:", title)
+          process.exit(0)
+        }
         fetch("http://ytptube.podman/api/history", {
           method: "POST",
           body: JSON.stringify({
@@ -35,6 +49,10 @@
           channel_id = "UCo1NRG1UDiCBx_20FanA5bw";
           folder = "asmr";
         };
+        kimitsutenka = {
+          channel_id = "UCZkZnbVO2bECxwcGlE9YOGg";
+          folder = "asmr";
+        };
       };
       tasks =
         with lib;
@@ -43,7 +61,7 @@
           nameValuePair ("ytptube-" + name) {
             rss = "https://www.youtube.com/feeds/videos.xml?channel_id=${value.channel_id}";
             accept_all = true;
-            exec.on_output.for_entries = ''${script} "{{url}}" "${value.folder}"'';
+            exec.on_output.for_entries = ''${script} "{{title}}" "{{url}}" "${value.folder}"'';
           }
         ) channels;
     in
