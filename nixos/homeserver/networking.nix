cb950d93d9e0e7fa47d8e2d2adcb132babfc0d01
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-12-09 14:26:59 +0700

    feat(homeserver): added public key for envy

--- nixos/homeserver/networking.nix
+++ nixos/homeserver/networking.nix
@@ -2,13 +2,6 @@
   # Unless we have multiple network cards, we don't need this to be true.
   networking.usePredictableInterfaceNames = false;
 
-  users.users.homeserver = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPO1aSG3/1vrgEPgK038tZ8+ipz3gZqr9hRT0JUteJXY tigor@fort"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/dGHD56+3qsLhUvmG4GeN8JrpYw7oGt0iQT+WkZzFu tigor@nexus"
-    ];
-  };
-
   # Use scripted networking (DHCP via dhcpcd)
   networking.interfaces.eth0.useDHCP = true;
 
--- nixos/homeserver/user.nix
+++ nixos/homeserver/user.nix
@@ -12,6 +12,11 @@
       "homeserver"
     ];
     shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPO1aSG3/1vrgEPgK038tZ8+ipz3gZqr9hRT0JUteJXY tigor@fort"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/dGHD56+3qsLhUvmG4GeN8JrpYw7oGt0iQT+WkZzFu tigor@nexus"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGUdNT+Pr015Li6Jp9cb1vCghd2C8EnecYwSC98qQCxl tigor@envy"
+    ];
   };
   users.groups.homeserver = { };
 }
