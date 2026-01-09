87fd2c04ea68bbfa7f481df4fc6fe1f1effbb2ee
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-01-09 19:29:12 +0700

    feat(grandboard): force SSH for Grand-Board repos
    Rewrite HTTPS URLs to SSH for all github.com/Grand-Board/* repositories.

--- nixos/environments/grandboard/home-manager/go.nix
+++ nixos/environments/grandboard/home-manager/go.nix
@@ -3,4 +3,9 @@
     enable = true;
     env.GOPRIVATE = [ "github.com/Grand-Board/*" ];
   };
+  programs.git.extraConfig = {
+    url."git@github.com:Grand-Board/" = {
+      insteadOf = "https://github.com/Grand-Board/";
+    };
+  };
 }
