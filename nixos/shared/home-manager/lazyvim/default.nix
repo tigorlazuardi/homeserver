00637950734cce62e3e1eb789018aff879036c98
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-01-20 20:35:22 +0700

    feat(neovim): added lazygit and opencode

--- nixos/shared/home-manager/lazyvim/default.nix
+++ nixos/shared/home-manager/lazyvim/default.nix
@@ -3,5 +3,9 @@
   programs.neovim.enable = true;
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/shared/home-manager/lazyvim/nvim";
-  home.packages = with pkgs; [ cargo ];
+  home.packages = with pkgs; [
+    cargo
+    lsof
+    statix
+  ];
 }
