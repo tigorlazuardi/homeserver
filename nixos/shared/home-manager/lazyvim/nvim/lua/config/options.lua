75c9b1fbe7c85ee5498d2812d1613922ce610537
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-01-28 12:33:38 +0700

    feat(neovim): added transparency support

--- nixos/shared/home-manager/lazyvim/nvim/lua/config/options.lua
+++ nixos/shared/home-manager/lazyvim/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.g.neovide then
+  vim.g.neovide_opacity = 0.5
+  vim.g.neovide_window_blurred = true
+  vim.g.experimental_layer_grouping = true
+end
