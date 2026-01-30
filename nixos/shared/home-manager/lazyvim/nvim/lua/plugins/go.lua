340e888fd5978f2d73649604c445b89473fdf155
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-01-30 16:08:33 +0700

    fix(go): use global golangci-lint config in nvim-lint

--- nixos/shared/home-manager/lazyvim/nvim/lua/plugins/go.lua
+++ nixos/shared/home-manager/lazyvim/nvim/lua/plugins/go.lua
@@ -1,20 +1,41 @@
+local home = os.getenv("HOME")
+
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      gopls = {
-        settings = {
-          gopls = {
-            usePlaceholders = false,
-            hints = {
-              assignVariableTypes = false,
-              compositeLiteralFields = false,
-              compositeLiteralTypes = false,
-              constantValues = false,
-              functionTypeParameters = false,
-              parameterNames = false,
-              rangeVariableTypes = false,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              usePlaceholders = false,
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
             },
+          },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        golangcilint = {
+          args = {
+            "run",
+            "--config",
+            home .. "/.golangci.yml",
+            "--out-format",
+            "json",
+            "--issues-exit-code=1",
           },
         },
       },
