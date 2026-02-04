3846c8fd7f410637052c7eb7e9a76172d5504f45
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-02-04 09:37:34 +0700

    feat(go): fix golangci lint v2 config

--- nixos/shared/home-manager/lazyvim/nvim/lua/plugins/go.lua
+++ nixos/shared/home-manager/lazyvim/nvim/lua/plugins/go.lua
@@ -18,6 +18,24 @@ return {
                 parameterNames = false,
                 rangeVariableTypes = false,
               },
+              analyses = {
+                -- disable shadow check (variable shadowing)
+                shadow = false,
+                -- disable all style/cosmetic checks (ST1xxx)
+                ST1000 = false, -- package comment
+                ST1003 = false, -- naming convention
+                ST1005 = false, -- error strings capitalization
+                ST1006 = false, -- receiver name
+                ST1012 = false, -- error var prefix
+                ST1016 = false, -- receiver name consistency
+                ST1017 = false, -- yoda conditions
+                ST1018 = false, -- string constant whitespace
+                ST1019 = false, -- duplicate imports
+                ST1020 = false, -- exported function comment
+                ST1021 = false, -- exported type comment
+                ST1022 = false, -- exported var comment
+                ST1023 = false, -- redundant type declaration
+              },
             },
           },
         },
@@ -29,14 +47,8 @@ return {
     opts = {
       linters = {
         golangcilint = {
-          args = {
-            "run",
-            "--config",
-            home .. "/.golangci.yml",
-            "--out-format",
-            "json",
-            "--issues-exit-code=1",
-          },
+          -- use default args from nvim-lint (auto-detects v1/v2)
+          -- global config ~/.golangci.yml is auto-discovered
         },
       },
     },
