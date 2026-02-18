a27995b496e37b56b372c1e06f0f14275f5c5ac6
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-02-18 13:02:41 +0700

    feat(nexus): update catppuccin

--- nixos/nexus/hardware/grub.nix
+++ nixos/nexus/hardware/grub.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   boot.loader = {
     systemd-boot.enable = false; # we will dual boot between NixOS and Windows
@@ -6,6 +7,8 @@
       useOSProber = true;
       efiSupport = true;
       device = "nodev";
+      configurationLimit = 50;
+      theme = pkgs.catppuccin-grub;
     };
   };
 }
