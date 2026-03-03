7a81c20f4549365437f52d7778090fa235f09b32
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-03-03 11:48:01 +0700

    feat(neovim): added guess-indent and tinyinlinediagnostics

--- nixos/shared/home-manager/lazyvim/nvim/lua/config/options.lua
+++ nixos/shared/home-manager/lazyvim/nvim/lua/config/options.lua
@@ -10,3 +10,5 @@ end
 
 -- Enable this option to avoid conflicts with Prettier.
 vim.g.lazyvim_prettier_needs_config = true
+
+vim.opt.tabstop = 4
---
+++ nixos/shared/home-manager/lazyvim/nvim/lua/plugins/guess-indent.lua
@@ -0,0 +1,7 @@
+return {
+  { "nmac427/guess-indent.nvim", opts = {} },
+  {
+    "nvim-treesitter",
+    opts = { indent = { enable = false } }, -- Disable annoying wrong indentation when creating new lines
+  },
+}
