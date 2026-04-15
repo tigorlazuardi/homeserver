e71766f78cc92177697b1aef3e64c38cef718f8b
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-04-15 15:27:56 +0700

    fix(immich): update image digest

--- nixos/homeserver/services/immich.nix
+++ nixos/homeserver/services/immich.nix
@@ -70,7 +70,7 @@ in
       mkdir -p ${volume}/valkey
     '';
   virtualisation.oci-containers.containers.immich-postgres = {
-    image = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0";
+    image = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0@sha256:bcf63357191b76a916ae5eb93464d65c07511da41e3bf7a8416db519b40b1c23";
     ip = "10.88.4.4";
     environment = {
       POSTGRES_USER = "immich";
