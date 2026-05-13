01eb7b2a8d9f57ea430c956c252a8081c3763d40
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-05-13 22:02:41 +0700

    feat(pi): added common dependencies

--- nixos/shared/home-manager/pi-coding-agent/default.nix
+++ nixos/shared/home-manager/pi-coding-agent/default.nix
@@ -5,9 +5,16 @@
   ...
 }:
 {
-  home.packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
-    pi
-  ];
+  home.packages =
+    with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system};
+    [
+      pi
+    ]
+    ++ (with pkgs; [
+      pnpm
+      bun
+      nodejs
+    ]);
 
   home.file.".pi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/shared/home-manager/pi-coding-agent/.pi";
