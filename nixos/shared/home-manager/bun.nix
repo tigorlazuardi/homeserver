8cf7478ad8998c83aaf92c8e137797531eae1c87
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-05-13 19:32:03 +0700

    feat(bun): removed fish

--- nixos/shared/home-manager/bun.nix
+++ nixos/shared/home-manager/bun.nix
@@ -10,8 +10,4 @@
   home.sessionPath = [
     "${config.home.homeDirectory}/.bun/bin"
   ];
-
-  programs.fish.interactiveShellInit = /* fish */ ''
-    fish_add_path ${config.home.homeDirectory}/.bun/bin
-  '';
 }
